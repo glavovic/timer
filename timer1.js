f944b3c9657112b50b1c08f6d91f2fdf0a0f4f96
Armin Glavovic <glavovic.armin@gmail.com>
2022-01-18 23:12:44 +0000

    filtered array

--- timer1.js
+++ timer1.js
@@ -2,6 +2,9 @@ argv = process.argv.slice(2)
 
 let arr = argv.map(x => parseInt(x))
 
+arr = arr.filter(x => Number.isInteger(x));
+
+console.log(arr)
 const timer = (arr) => {
   for (let num of arr) {
     if(!num === NaN || !num >= 0) {
